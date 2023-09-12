f300f5e60b2c3d7581f8857d5bcaac5d908cfea8
Gonzalo <gorodriguez@frba.utn.edu.ar>
2023-09-12 16:51:46 -0300

    agregue row_number

--- src/main/scala/org/novakorp/com/cambio_precio.scala
+++ src/main/scala/org/novakorp/com/cambio_precio.scala
@@ -13,19 +13,37 @@ object cambio_precio extends SparkSessionWrapper  {
   def CalcularDataFrame(df_movimientos: DataFrame , df_costo_unificado: DataFrame , df_stock: DataFrame, df_articulos: DataFrame, fecha_inicial: String, fecha_final: String) : DataFrame = {
 
     // Definir una ventana ordenada por la columna "fecha_vigencia_desde" para cada "codigo_barras"
-    val ventana = Window.partitionBy("barras").orderBy("fecha_vigencia_desde")
+    val ventana = Window.partitionBy("barras").orderBy("id_costo_unificado")
+
     // Costo unificado y en cada fila su costo anterior
     val df_costo_unificado_con_anterior = df_costo_unificado.withColumn("costo_anterior", when(lag("costo", 1).over(ventana).isNull, col("costo")).otherwise(lag("costo", 1).over(ventana)))
-    val df_costo_unificado_periodo=df_costo_unificado_con_anterior.filter(f"fecha_vigencia_desde BETWEEN ${fecha_inicial} AND ${fecha_final}")
+
+    // Definir una ventana particionada por "fecha_vigencia_desde" y "barras"
+    // y ordenada por "id_costo_unificado" en orden descendente
+    val windowSpec = Window.partitionBy("fecha_vigencia_desde", "barras").orderBy(desc("id_costo_unificado"))
+
+    // Agregar una columna de número de fila basada en la ventana
+    val rankedDf = df_costo_unificado_con_anterior.withColumn("row_num", row_number().over(windowSpec))
+
+    // Filtrar las filas donde row_num es igual a 1 (la primera fila en cada grupo)
+    val resultDf = rankedDf.filter(col("row_num") === 1).drop("row_num")
+
+    val df_costo_unificado_periodo=resultDf.filter(f"fecha_vigencia_desde BETWEEN ${fecha_inicial} AND ${fecha_final}")
+
     val df_sin_mov_cu_temp=df_costo_unificado_periodo.as("cu").join(df_movimientos,(df_costo_unificado_periodo("barras")===df_movimientos("codigo_barra")),"left_outer").select(col("cu.*"),col("codigo_articulo").as("codigo")).withColumnRenamed("fecha_vigencia_desde","fecha_stock")
+
     // Join con articulos para agregar Codigo.
     val df_sin_mov_cu=df_sin_mov_cu_temp.as("cu").join(df_articulos,(df_sin_mov_cu_temp("barras")===df_articulos("codigo_barra")),"inner").select(col("cu.*"),col("codigo"))
+
     // Se filtra el stock para el día de hoy (ya que al haber movimientos, se realizó el cálculo para tener el nuevo stock del día)
     val df_stock_filtrado = df_stock.filter(col("fecha_stock") <= fecha_final)
+
     // Se agrupa el stock del día y se trae la fecha más reciente (stock actual)
     val df_stock_agrupado = df_stock_filtrado.groupBy("codigo").agg(max("fecha_stock").as("max_fecha_stock"))//.orderBy(desc("max_fecha_stock"))
+
     // Joineamos la fecha más actual con el stock, para traer el valor de existencia junto con el código y la fecha_stock
     val df_join_stock = df_stock_agrupado.join(df_stock, Seq("codigo"), "inner").filter(col("fecha_stock") === col("max_fecha_stock")).select(col("codigo"), col("existencia"), col("fecha_stock"))
+
     // Se joinea el stock actual junto con los artículos con movimientos y sus precios, a su vez se calcula RxT
     val df_pre_final: DataFrame = df_join_stock.as("stock").join(df_sin_mov_cu.as("precios"), Seq("codigo"), "inner").withColumn("movimientos_agrupados", lit(0)).withColumn("resultado_por_tenencia", ((col("costo_unitario") - col("costo_anterior")) * col("existencia"))).select(col("codigo"), col("barras"), col("precios.fecha_stock"), col("movimientos_agrupados"), col("stock.existencia").as("total_unidades"), col("costo_unitario"), col("costo_anterior"), col("precio_actual_mayorista"), col("precio_actual_minorista"), col("resultado_por_tenencia")).distinct
 
--- src/main/scala/org/novakorp/com/con_mov_con_cu.scala
+++ src/main/scala/org/novakorp/com/con_mov_con_cu.scala
@@ -12,13 +12,23 @@ object con_mov_con_cu extends SparkSessionWrapper  {
         val df_mov_agrupado: DataFrame = df_movimientos.groupBy("codigo_articulo", "fecha", "codigo_barra").agg(sum(col("cantidad_movimiento") * col("multiplicador_stock").cast("int")).as("movimientos_agrupados"))
 
         // Definir una ventana ordenada por la columna "fecha_vigencia_desde" para cada "codigo_barras"
-        val ventana = Window.partitionBy("barras").orderBy("fecha_vigencia_desde")
+        val ventana = Window.partitionBy("barras").orderBy("id_costo_unificado")
 
         // Costo unificado y en cada fila su costo anterior
         val df_costo_unificado_con_anterior = df_costo_unificado.withColumn("costo_anterior", when(lag("costo", 1).over(ventana).isNull, col("costo")).otherwise(lag("costo", 1).over(ventana))).filter(f"fecha_vigencia_desde BETWEEN ${fecha_inicial} AND ${fecha_final}")
 
+        // Definir una ventana particionada por "fecha_vigencia_desde" y "barras"
+        // y ordenada por "id_costo_unificado" en orden descendente
+        val windowSpec = Window.partitionBy("fecha_vigencia_desde", "barras").orderBy(desc("id_costo_unificado"))
+
+        // Agregar una columna de número de fila basada en la ventana
+        val rankedDf = df_costo_unificado_con_anterior.withColumn("row_num", row_number().over(windowSpec))
+
+        // Filtrar las filas donde row_num es igual a 1 (la primera fila en cada grupo)
+        val resultDf = rankedDf.filter(col("row_num") === 1).drop("row_num")
+
         // Se joinean los artículos con movimientos y los con cambio de precio
-        val df_con_mov_con_cu: DataFrame = df_costo_unificado_con_anterior.as("actual").join(df_mov_agrupado, (col("barras") === col("codigo_barra")) && (col("fecha") === col("fecha_vigencia_desde")), "inner").select(col("actual.*"), col("codigo_articulo").as("codigo"), col("movimientos_agrupados"))
+        val df_con_mov_con_cu: DataFrame = resultDf.as("actual").join(df_mov_agrupado, (col("barras") === col("codigo_barra")) && (col("fecha") === col("fecha_vigencia_desde")), "inner").select(col("actual.*"), col("codigo_articulo").as("codigo"), col("movimientos_agrupados"))
 
         // Se filtra el stock para el periodo correspondiente
         val df_stock_filtrado = df_stock.filter(f"fecha_stock BEETWEEN ${fecha_inicial} AND ${fecha_final}")
