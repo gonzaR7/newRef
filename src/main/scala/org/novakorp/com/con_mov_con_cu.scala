8e2911e600f29a8b89e850ecfdbd6bbf70243d1b
Gonzalo <gorodriguez@frba.utn.edu.ar>
2023-09-14 15:04:05 -0300

    actualizacion y validacion sin_mov_sin_cu

--- src/main/scala/org/novakorp/com/con_mov_con_cu.scala
+++ src/main/scala/org/novakorp/com/con_mov_con_cu.scala
@@ -34,7 +34,7 @@ object con_mov_con_cu extends SparkSessionWrapper  {
         val df_stock_filtrado = df_stock.filter(f"fecha_stock BEETWEEN ${fecha_inicial} AND ${fecha_final}")
 
         // Se joinea el stock actual junto con los artículos con movimientos y sus precios, a su vez se calcula RxT
-        val df_pre_final: DataFrame = df_stock_filtrado.as("stock").join(df_con_mov_con_cu.as("precios"),(df_stock_filtrado("codigo")===df_con_mov_con_cu("codigo"))&&(df_stock_filtrado("fecha_stock")===df_con_mov_con_cu("fecha_vigencia_desde")) , "inner").withColumn("resultado_por_tenencia", ((col("costo_unitario") - col("costo_anterior")) * col("existencia"))).select(col("codigo"), col("barras"), col("precios.fecha_stock"), col("movimientos_agrupados"), col("stock.existencia").as("total_unidades"), col("costo_unitario"), col("costo_anterior"), col("precio_mayorista").as("precio_actual_mayorista"), col("precio_minorista").as("precio_actual_minorista"), col("resultado_por_tenencia"))//.distinct
+        val df_pre_final: DataFrame = df_stock_filtrado.as("stock").join(df_con_mov_con_cu.as("precios"),(df_stock_filtrado("codigo")===df_con_mov_con_cu("codigo"))&&(df_stock_filtrado("fecha_stock")===df_con_mov_con_cu("fecha_vigencia_desde")) , "inner").withColumn("resultado_por_tenencia", ((col("costo_unitario") - col("costo_anterior")) * col("existencia"))).select(col("codigo"), col("barras"), col("precios.fecha_stock"), col("movimientos_agrupados"), col("stock.existencia").as("total_unidades"), col("costo_unitario"), col("costo_anterior"), col("precio_mayorista").as("precio_actual_mayorista"), col("precio_minorista").as("precio_actual_minorista"), col("resultado_por_tenencia")).distinct
 
         df_pre_final
     }
--- src/main/scala/org/novakorp/com/sin_mov_ni_costo.scala
+++ src/main/scala/org/novakorp/com/sin_mov_ni_costo.scala
@@ -32,18 +32,19 @@ object sin_mov_ni_costo extends SparkSessionWrapper  {
         .groupBy(df_articulos_no_movimiento_no_costo("codigo_barra"),df_articulos_no_movimiento_no_costo("fecha")).agg(max(df_costo_unificado_con_anterior("fecha_vigencia_desde"))
         .alias("max_fecha_vigencia"))
 
-    //PARA MI DEBERIA SER ASI: LE AGREGO LA FECHA EN df_costo_unificado_actual DEL df_articulos_no_movimiento_no_costo Y LUEGO EN df_precios JOINEO POR BARRAS Y FECHA
       val df_costo_unificado_actual = joinedDF.join(df_costo_unificado_con_anterior.as("cu"),(joinedDF("codigo_barra") === df_costo_unificado_con_anterior("barras")) && (joinedDF("max_fecha_vigencia") === df_costo_unificado_con_anterior("fecha_vigencia_desde")),"inner").select(col("cu.*"),joinedDF("fecha").as("fecha_df")).distinct()
 
       val df_precios = df_costo_unificado_actual.join(df_articulos_no_movimiento_no_costo, (df_costo_unificado_actual("barras") === df_articulos_no_movimiento_no_costo("codigo_barra")) && (df_costo_unificado_actual("fecha_df") === df_articulos_no_movimiento_no_costo("fecha")), "inner").select(col("codigo"), col("barras"), df_articulos_no_movimiento_no_costo("fecha").as("fecha_stock"), col("costo").as("costo_unitario"), col("costo_anterior"), col("precio_mayorista").as("precio_actual_mayorista"), col("precio_minorista").as("precio_actual_minorista"))
 
-        // Se filtra el stock para el día de hoy (ya que al haber movimientos, se realizó el cálculo para tener el nuevo stock del día)
-      val df_stock_filtrado = df_stock.filter(f"fecha_stock BETWEEN ${fecha_inicial} AND ${fecha_final}")
+      val df_precios_con_max_stock = df_precios.join(df_stock,(df_precios("codigo") === df_stock("codigo")) && (df_stock("fecha_stock") < df_precios("fecha_stock")),"inner").groupBy(df_precios("codigo"),df_precios("fecha_stock")).agg(max(df_stock("fecha_stock")).alias("max_fecha_stock"))
 
-        // Se joinea el stock actual junto con los artículos con movimientos y sus precios, a su vez se calcula RxT
-      val df_pre_final = df_stock_filtrado.as("stock").join(df_precios.as("precios"), Seq("codigo","fecha_stock"), "inner").withColumn("resultado_por_tenencia",lit(0)).select(col("codigo"), col("barras"), col("fecha_stock"), col("movimientos_agrupados").as("movimientos_agrupados"), col("stock.existencia").as("total_unidades"), col("costo_unitario"), col("costo_anterior"), col("precio_actual_mayorista"), col("precio_actual_minorista"), col("resultado_por_tenencia")).distinct
+      val df_sin_mov_sin_cu_max_stock = df_precios_con_max_stock.join(df_precios, Seq("codigo","fecha_stock"), "inner").select(df_precios("barras"),df_precios("fecha_stock"), col("costo_unitario"),col("costo_anterior"), col("precio_actual_minorista"), col("precio_actual_mayorista"),df_precios_con_max_stock("max_fecha_stock"),df_precios_con_max_stock("codigo"))
 
-      df_articulos_no_movimiento_no_costo
+      val df_join_stock = df_sin_mov_sin_cu_max_stock.join(df_stock, (df_sin_mov_sin_cu_max_stock("codigo")===df_stock("codigo")&&(df_sin_mov_sin_cu_max_stock("max_fecha_stock")===df_stock("fecha_stock"))), "inner").select(df_sin_mov_sin_cu_max_stock("barras"),df_sin_mov_sin_cu_max_stock("costo_unitario"),df_sin_mov_sin_cu_max_stock("costo_anterior"),df_sin_mov_sin_cu_max_stock("precio_actual_minorista"),df_sin_mov_sin_cu_max_stock("precio_actual_mayorista"),df_stock("codigo"), df_stock("existencia"), df_sin_mov_sin_cu_max_stock("fecha_stock"))
+
+      val df_pre_final = df_join_stock.withColumn("movimientos_agrupados", lit(0)).withColumn("resultado_por_tenencia",lit(0)).select(col("total.codigo"), col("barras"), col("fecha_stock"), col("movimientos_agrupados"), col("existencia").as("total_unidades"), col("costo_unitario"), col("costo_anterior"), col("precio_actual_mayorista"), col("precio_actual_minorista"), col("resultado_por_tenencia"))
+
+      df_pre_final
 
   }
 }
