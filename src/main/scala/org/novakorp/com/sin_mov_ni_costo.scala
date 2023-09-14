6af768e75a4a7332b99d1dee88106d13c19294ae
Gonzalo <gorodriguez@frba.utn.edu.ar>
2023-09-14 00:10:52 -0300

    varios cambios mejora nashee

--- src/main/scala/org/novakorp/com/sin_mov_ni_costo.scala
+++ src/main/scala/org/novakorp/com/sin_mov_ni_costo.scala
@@ -28,12 +28,27 @@ object sin_mov_ni_costo extends SparkSessionWrapper  {
 
         //Para cada codigo de barra con su fecha me traigo la fecha de su ultimo cambio de precio
       val joinedDF = df_articulos_no_movimiento_no_costo
-        .join(df_costo_unificado_con_anterior,(df_articulos_no_movimiento_no_costo("codigo_barra") === df_costo_unificado_con_anterior("barras")) && (df_costo_unificado_con_anterior("fecha_vigencia_desde") < df_mov_agrupado("fecha")),"inner")
+        .join(df_costo_unificado_con_anterior,(df_articulos_no_movimiento_no_costo("codigo_barra") === df_costo_unificado_con_anterior("barras")) && (df_costo_unificado_con_anterior("fecha_vigencia_desde") < df_articulos_no_movimiento_no_costo("fecha")),"inner")
         .groupBy(df_articulos_no_movimiento_no_costo("codigo_barra"),df_articulos_no_movimiento_no_costo("fecha")).agg(max(df_costo_unificado_con_anterior("fecha_vigencia_desde"))
         .alias("max_fecha_vigencia"))
 
-        //Para tener los campos de df_costo_unificado_con_anterior
-      val df_costo_unificado_actual = joinedDF.join(df_costo_unificado_con_anterior.as("cu"),(joinedDF("codigo_barra") === df_costo_unificado_con_anterior("barras")) && (joinedDF("max_fecha_vigencia") === df_costo_unificado_con_anterior("fecha_vigencia_desde")),"inner").select(col("cu.*"))
+        //Para tener los campos de df_costo_unificado_con_anterior - COMO HAY VARIOS BARRIAS IGUALES CON DISTINTAS FECHAS Y PUEDEN TENER DISTINTOS ULTIMA FECHA_VIG_DESDE.
+      val df_costo_unificado_actual = joinedDF.join(df_costo_unificado_con_anterior.as("cu"),(joinedDF("codigo_barra") === df_costo_unificado_con_anterior("barras")) && (joinedDF("max_fecha_vigencia") === df_costo_unificado_con_anterior("fecha_vigencia_desde")),"inner").select(col("cu.*")).distinct()
+
+    //PARA MI DEBERIA SER ASI: LE AGREGO LA FECHA EN df_costo_unificado_actual DEL df_articulos_no_movimiento_no_costo Y LUEGO EN df_precios JOINEO POR BARRAS Y FECHA
+//      val df_costo_unificado_actual = joinedDF.join(df_costo_unificado_con_anterior.as("cu"),(joinedDF("codigo_barra") === df_costo_unificado_con_anterior("barras")) && (joinedDF("max_fecha_vigencia") === df_costo_unificado_con_anterior("fecha_vigencia_desde")),"inner").select(col("cu.*"),joinedDF("fecha").as("fecha_df")).distinct()
+//      val df_precios = df_costo_unificado_actual.join(df_articulos_no_movimiento_no_costo, (df_costo_unificado_actual("barras") === df_articulos_no_movimiento_no_costo("codigo_barra")) && (df_costo_unificado_actual("fecha_df") === df_articulos_no_movimiento_no_costo("fecha")), "inner").select(col("codigo_articulo").as("codigo"), col("barras"), df_articulos_no_movimiento_no_costo("fecha").as("fecha_stock"), col("movimientos_agrupados"), col("costo").as("costo_unitario"), col("costo_anterior"), col("precio_mayorista").as("precio_actual_mayorista"), col("precio_minorista").as("precio_actual_minorista"))
+    //ESTE BLOQUE APLICARIA PARA "con_movimientos" TAMBIEN, CHEQUEAR EH
+
+
+    //CORREGIR EL SELECT (para sin_mov_sin_costo, ya que lo copie de con_movimientos) Y VERIRIFICAR IGUALMENTE DE ACA PARA ABAJO
+      val df_precios = df_costo_unificado_actual.join(df_articulos_no_movimiento_no_costo, (df_costo_unificado_actual("barras") === df_articulos_no_movimiento_no_costo("codigo_barra")), "inner").select(col("codigo_articulo").as("codigo"), col("barras"), df_articulos_no_movimiento_no_costo("fecha").as("fecha_stock"), col("movimientos_agrupados"), col("costo").as("costo_unitario"), col("costo_anterior"), col("precio_mayorista").as("precio_actual_mayorista"), col("precio_minorista").as("precio_actual_minorista"))
+
+        // Se filtra el stock para el día de hoy (ya que al haber movimientos, se realizó el cálculo para tener el nuevo stock del día)
+      val df_stock_filtrado = df_stock.filter(f"fecha_stock BETWEEN ${fecha_inicial} AND ${fecha_final}")
+
+        // Se joinea el stock actual junto con los artículos con movimientos y sus precios, a su vez se calcula RxT
+      val df_pre_final = df_stock_filtrado.as("stock").join(df_precios.as("precios"), Seq("codigo","fecha_stock"), "inner").withColumn("resultado_por_tenencia",lit(0)).select(col("codigo"), col("barras"), col("fecha_stock"), col("movimientos_agrupados").as("movimientos_agrupados"), col("stock.existencia").as("total_unidades"), col("costo_unitario"), col("costo_anterior"), col("precio_actual_mayorista"), col("precio_actual_minorista"), col("resultado_por_tenencia")).distinct
 
       df_articulos_no_movimiento_no_costo
 
