5b6f2f83c393044aadd6cfb949794f222428c5e8
Gonzalo <gorodriguez@frba.utn.edu.ar>
2023-09-14 10:32:18 -0300

    varios cambios mejora nashee

--- src/main/scala/org/novakorp/com/con_movimientos.scala
+++ src/main/scala/org/novakorp/com/con_movimientos.scala
@@ -21,7 +21,7 @@ object con_movimientos extends SparkSessionWrapper  {
     val df_mov_agrupado: DataFrame = df_con_mov_filtrado.groupBy("codigo_articulo", "fecha", "codigo_barra").agg(sum(col("cantidad_movimiento") * col("multiplicador_stock").cast("int")).as("movimientos_agrupados"))
 
     // Definir una ventana ordenada por la columna "fecha_vigencia_desde" para cada "codigo_barras"
-    val ventana = Window.partitionBy("barras").orderBy("fecha_vigencia_desde")
+    val ventana = Window.partitionBy("barras").orderBy("id_costo_unificado")
 
     // Costo unificado y en cada fila su costo anterior
     val df_costo_unificado_con_anterior = df_costo_unificado.withColumn("costo_anterior", when(lag("costo", 1).over(ventana).isNull, col("costo")).otherwise(lag("costo", 1).over(ventana)))
@@ -33,13 +33,13 @@ object con_movimientos extends SparkSessionWrapper  {
 
     val df_costo_unificado_actual = joinedDF.join(df_costo_unificado_con_anterior.as("cu"),(joinedDF("codigo_barra") === df_costo_unificado_con_anterior("barras")) && (joinedDF("max_fecha_vigencia") === df_costo_unificado_con_anterior("fecha_vigencia_desde")),"inner").select(col("cu.*")).distinct()
 
-    val df_precios: DataFrame = df_costo_unificado_actual.join(df_mov_agrupado, (df_costo_unificado_actual("barras") === df_mov_agrupado("codigo_barra")), "inner").select(col("codigo_articulo").as("codigo"), col("barras"), df_mov_agrupado("fecha").as("fecha_stock"), col("movimientos_agrupados"), col("costo").as("costo_unitario"), col("costo_anterior"), col("precio_mayorista").as("precio_actual_mayorista"), col("precio_minorista").as("precio_actual_minorista"))
+    val df_precios = df_costo_unificado_actual.join(df_mov_agrupado, (df_costo_unificado_actual("barras") === df_mov_agrupado("codigo_barra")), "inner").select(col("codigo_articulo").as("codigo"), col("barras"), df_mov_agrupado("fecha").as("fecha_stock"), col("movimientos_agrupados"), col("costo").as("costo_unitario"), col("costo_anterior"), col("precio_mayorista").as("precio_actual_mayorista"), col("precio_minorista").as("precio_actual_minorista"))
 
     // Se filtra el stock para el día de hoy (ya que al haber movimientos, se realizó el cálculo para tener el nuevo stock del día)
     val df_stock_filtrado = df_stock.filter(f"fecha_stock BETWEEN ${fecha_inicial} AND ${fecha_final}")
 
     // Se joinea el stock actual junto con los artículos con movimientos y sus precios, a su vez se calcula RxT
-    val df_pre_final: DataFrame = df_stock_filtrado.as("stock").join(df_precios.as("precios"), Seq("codigo","fecha_stock"), "inner").withColumn("resultado_por_tenencia",lit(0)).select(col("codigo"), col("barras"), col("fecha_stock"), col("movimientos_agrupados").as("movimientos_agrupados"), col("stock.existencia").as("total_unidades"), col("costo_unitario"), col("costo_anterior"), col("precio_actual_mayorista"), col("precio_actual_minorista"), col("resultado_por_tenencia")).distinct
+    val df_pre_final = df_stock_filtrado.as("stock").join(df_precios.as("precios"), Seq("codigo","fecha_stock"), "inner").withColumn("resultado_por_tenencia",lit(0)).select(col("codigo"), col("barras"), col("fecha_stock"), col("movimientos_agrupados").as("movimientos_agrupados"), col("stock.existencia").as("total_unidades"), col("costo_unitario"), col("costo_anterior"), col("precio_actual_mayorista"), col("precio_actual_minorista"), col("resultado_por_tenencia")).distinct
 
     df_pre_final
 
